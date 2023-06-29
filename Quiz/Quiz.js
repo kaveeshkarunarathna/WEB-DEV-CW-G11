b4c7de2fb8ec2f262b1c66502808ad4ea7d1328c
Randima Fernando <randimatharusha@gmail.com>
2023-06-29 23:09:50 +0530

    scripting the Question page and Adding questions

---
+++ Quiz/Quiz.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Which player holds the record for the highest individual score in Test cricket?",
+        answer: "B. Brian Lara",
+        Options: [
+            "A. Sachin Tendulkar",
+            "B. Brian Lara",
+            "C. Ricky Ponting",
+            "D. Don Bradman"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Who is the leading run-scorer in One Day International (ODI) cricket?",
+        answer: "A. Sachin Tendulkar",
+        Options: [
+            "A. Sachin Tendulkar",
+            "B. Virat Kohli",
+            "C. Ricky Ponting",
+            "D. Kumar Sangakkara"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which country has won the most Cricket World Cup titles?",
+        answer: "A. Australia",
+        Options: [
+            "A. Australia",
+            "B. India",
+            "C. West Indies",
+            "D. England"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Who is the fastest bowler to reach 100 wickets in Test cricket?",
+        answer: "A. Dale Steyn",
+        Options: [
+            "A. Dale Steyn",
+            "B. Shane Warne",
+            "C. Ravichandran Ashwin",
+            "D. Malcolm Marshall"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which player has taken the most catches in international cricket?",
+        answer: "C. Ricky Ponting",
+        Options: [
+            "A. Mahela Jayawardene",
+            "B. Rahul Dravid",
+            "C. Ricky Ponting",
+            "D. Jacques Kallis"
+        ]
+    },
+];
---
+++ Quiz/Script.js
@@ -0,0 +1,43 @@
+const startBtn = document.querySelector('.start-btn');
+const popupInfo = document.querySelector('.popup-info');
+const exitBtn = document.querySelector('.exit-btn');
+const main = document.querySelector('.main');
+const continueBtn = document.querySelector('.continue-btn');
+const quizSection = document.querySelector('.quiz-section');
+const quizBox = document.querySelector('.quiz-box');
+
+startBtn.onclick =() => {
+    popupInfo.classList.add('active');
+    main.classList.add('active');
+}
+
+exitBtn.onclick =() => {
+    popupInfo.classList.remove('active');
+    main.classList.remove('active');
+}
+
+continueBtn.onclick =() => {
+    quizSection.classList.add('active');
+    quizBox.classList.add('active');
+    popupInfo.classList.remove('active');
+    main.classList.remove('active');
+
+    showQuestions(0);
+}
+
+let questionCount = 0;
+
+const nextBtn = document.querySelector('.next-btn');
+
+nextBtn.onclick =() => {
+    questionCount++;
+    showQuestions(questionCount);
+
+    showQuestions(0);
+}
+
+//getting questions and options from array
+function showQuestions(index) {
+    const questionText = document.querySelector('.question-text');
+    questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
+}
