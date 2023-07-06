c3e33ebc8d1e0dfcae266dab1347c8eded3495fc
Randima Fernando <randimatharusha@gmail.com>
2023-07-06 18:57:02 +0530

    add countdown to presentationpage

--- Presentation Page/presentationPage.js
+++ Presentation Page/presentationPage.js
@@ -1,9 +1,31 @@
+const remainingTime = document.querySelector('.countdown');
 
- function transitionToMainPage() {
-    setTimeout(function() {
-      window.location.href = "/Main Page/mainPage.html";
-    }, 4000); 
-  }
+remainingTime.classList.add('active');
+startCountdown();
+
+function startCountdown() {
+  let countdown = 8;
+  remainingTime.textContent = countdown;
+  let timer = setInterval(function() {
+    countdown--;
+    remainingTime.textContent = countdown;
+    if (countdown <= 0) {
+      clearInterval(timer);
+      navigateToMainPage();
+    }
+  }, 1000);
+}
+
+function navigateToMainPage() {
+  window.location.href = "/main Page/mainPage.html";
+}
+
+
+//function transitionToMainPage() {
+//  setTimeout(function() {
+//    window.location.href = "/Main Page/mainPage.html";
+//  }, 4000); 
+//}
   
   
   // window.onload = function() {
