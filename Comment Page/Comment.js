ead08c4501714a328ecfd5fb8f02cdb3732d5a30
KaveeshAxis <101452117+kaveeshkarunarathna@users.noreply.github.com>
2023-07-23 23:52:08 +0530

    fixed issue

--- Comment Page/Comment.js
+++ Comment Page/Comment.js
@@ -33,10 +33,9 @@ function validateForm() {
     return false;
   }
   
-  else {showThankYouMessage();
-  submitForm();
+  showThankYouMessage();
   return false;
-  }
+
 }
 
 function clearErrors() {
@@ -57,15 +56,27 @@ function showThankYouMessage() {
 }
 
 function submitForm() {
+
+  const satisfaction = document.forms["feedbackForm"]["satisfaction"].value;
+  const ratingExplanation = document.forms["feedbackForm"]["ratingExplanation"].value;
+  const favoriteFeature = document.forms["feedbackForm"]["favoriteFeature"].value;
+  const recommendation = document.forms["feedbackForm"]["recommendation"].value;
+  const email = document.forms["feedbackForm"]["email"].value;
+
+
+  // Get the email and other form data (you may need to add other variables as needed)
   const subject = "Feedback from " + email;
+  // ... other form data ...
+
+  // Create the mailto link
   const emailBody =
     `Satisfaction: ${satisfaction}%0D%0A` +
     `Rating Explanation: ${ratingExplanation}%0D%0A` +
     `Favorite Feature: ${favoriteFeature}%0D%0A` +
     `Recommendation: ${recommendation}%0D%0A`;
 
-  const mailtoLink = `mailto:kaveesh.20223173@iit.ac.lk?subject=${subject}&body=${emailBody}`;
+  const mailtoLink = `mailto:kaveeshbc@gmail.com?subject=${subject}&body=${emailBody}`;
   
-  // Open the mailto link in a new window
-  window.open(mailtoLink);
+  // Open the mailto link in a new tab
+  window.open(mailtoLink, '_blank');
 }
